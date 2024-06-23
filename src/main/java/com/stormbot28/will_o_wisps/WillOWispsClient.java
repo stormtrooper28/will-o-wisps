8136f88f5c37b109f280166ad154d92a85d5cc18
stormbot28 <stormtrooper28@live.com>
2024-06-23 18:20:15 -0400

    Initial commit & push

---
+++ src/main/java/com/stormbot28/will_o_wisps/WillOWispsClient.java
@@ -0,0 +1,10 @@
+package com.stormbot28.will_o_wisps;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WillOWispsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/stormbot28/will_o_wisps/WillOWispsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.stormbot28.will_o_wisps;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WillOWispsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
